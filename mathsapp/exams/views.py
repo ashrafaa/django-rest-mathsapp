7776c0832b38652d9ba6ecb19bd04960917db3ae
Ashraf Anwar <ashraf.a@spartadigital.co.uk>
2018-11-28 18:34:15 +0000

    feat: add filter to views

--- mathsapp/exams/views.py
+++ mathsapp/exams/views.py
@@ -1,10 +1,13 @@
 from rest_framework import status, generics
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Exam, Question, Answer
 from .serializers import *
 
 class answer_list(generics.ListCreateAPIView):
     queryset = Answer.objects.all()
     serializer_class = AnswerSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('question__text', 'solutionValue')
 
 class answer_detail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Answer.objects.all()
@@ -13,6 +16,8 @@ class answer_detail(generics.RetrieveUpdateDestroyAPIView):
 class question_list(generics.ListCreateAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('exam__name', 'difficulty')
 
 class question_detail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Question.objects.all()
@@ -21,6 +26,8 @@ class question_detail(generics.RetrieveUpdateDestroyAPIView):
 class exam_list(generics.ListCreateAPIView):
     queryset = Exam.objects.all()
     serializer_class = ExamSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('name', 'passPercentage')
 
 class exam_detail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Exam.objects.all()
--- mathsapp/settings.py
+++ mathsapp/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
+}
