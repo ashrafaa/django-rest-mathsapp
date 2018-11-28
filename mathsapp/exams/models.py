5a73ac05a7a412cf6166dd08c30ec83e66122dc1
Ashraf Anwar <ashraf.a@spartadigital.co.uk>
2018-11-28 19:42:43 +0000

    feat: update url and model - add in validators & choices to attribute

--- mathsapp/exams/models.py
+++ mathsapp/exams/models.py
@@ -1,25 +1,51 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Exam(models.Model):
     name = models.CharField(max_length=30)
-    passPercentage = models.FloatField() # expected value: 0 - 100
+    passPercentage = models.IntegerField(
+        default=80,
+        validators=[MaxValueValidator(100), MinValueValidator(0)]
+    ) # expected value: 0 - 100
 
     def __str__(self):
         return self.name
 
 class Question(models.Model):
+    DIFFICULTY_LEVELS = (
+        ('VH', 'Very Hard'),
+        ('HA', 'Hard'),
+        ('ME', 'Medium'),
+        ('EA', 'Easy'),
+        ('VE', 'Very Easy')
+    )
+
     text = models.CharField(max_length=255)
-    difficulty = models.IntegerField() # value: 1 (very easy) to 5 (very hard)
+    difficulty = models.CharField(
+        max_length=10,
+        choices=DIFFICULTY_LEVELS,
+        default='ME'
+    ) # value: 1 (very easy) to 5 (very hard)
     exam = models.ForeignKey(Exam, on_delete=models.CASCADE, related_name='questions')
 
     def __str__(self):
-        return "Question: %s (%d)" % (self.text, self.difficulty)
+        return "Question: %s (%s)" % (self.text, self.difficulty)
 
 class Answer(models.Model):
+    SOLUTION_VALUE = (
+        ('CO', 'Correct'),
+        ('AC', 'Acceptable'),
+        ('WR', 'Wrong')
+    )
+
     text = models.CharField(max_length=255)
-    solutionValue = models.IntegerField() # value: 0 (total wrong), 1 (not exact), 2 (correct)
+    solutionValue = models.CharField(
+        max_length=10,
+        choices=SOLUTION_VALUE,
+        default='WR'
+    ) # value: 0 (total wrong), 1 (not exact), 2 (correct)
     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
 
     def __str__(self):
-        return "Answer: %s (%d)" % (self.text, self.solutionValue)
+        return "Answer: %s (%s)" % (self.text, self.solutionValue)
 
--- mathsapp/urls.py
+++ mathsapp/urls.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('mathsapp.exams.urls')),
+    url(r'^api/v1/', include('mathsapp.exams.urls')),
 ]
